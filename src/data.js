a89ee1ba5a5470145056c371eb99e66d1f413f92
seki <seki@fringe81.com>
2015-06-29 11:16:21 +0900

    データファイル追加

---
+++ src/data.js
@@ -0,0 +1,38 @@
+module.exports = [
+  {
+    title: "Captain America",
+    src: "../imgs/captain-america.jpg",
+    desc: `Captain America is a fictional superhero appearing in American comic books published by Marvel Comics.
+    Created by cartoonists Joe Simon and Jack Kirby, the character first appeared in Captain America Comics #1 (cover dated March 1941) from Timely Comics, a predecessor of Marvel Comics.
+    Captain America was designed as a patriotic supersoldier who often fought the Axis powers of World War II and was Timely Comics' most popular character during the wartime period.
+    The popularity of superheroes waned following the war and the Captain America comic book was discontinued in 1950,
+    with a short-lived revival in 1953. Since Marvel Comics revived the character in 1964, Captain America has remained in publication.`
+  },
+  {
+    title: "Ironman",
+    src: "../imgs/ironman.jpg",
+    desc: `Iron Man (Tony Stark) is a fictional superhero appearing in American comic books published by Marvel Comics, as well as its associated media.
+    The character was created by writer-editor Stan Lee, developed by scripter Larry Lieber, and designed by artists Don Heck and Jack Kirby.
+    He made his first appearance in Tales of Suspense #39 (cover dated March 1963).
+    An American billionaire playboy, business magnate, and ingenious engineer, Tony Stark suffers a severe chest injury during a kidnapping in which his captors attempt to force him to build a weapon of mass destruction.
+    He instead creates a powered suit of armor to save his life and escape captivity. Later, Stark augments his suit with weapons and other technological devices he designed through his company, Stark Industries.`
+  },
+  {
+    title: "Mighty Thor",
+    src: "../imgs/thor.jpg",
+    desc: `Thor is a fictional superhero that appears in American comic books published by Marvel Comics.
+    The character, based on the Norse mythological deity of the same name,
+    is the Asgardian god of thunder and possesses the enchanted hammer Mjolnir,
+    which grants him the ability of flight and weather manipulation amongst his other superhuman attributes.
+    Debuting in the Silver Age of Comic Books, the character first appeared in Journey into Mystery #83 (Aug. 1962)
+    and was created by editor-plotter Stan Lee, scripter Larry Lieber, and penciller-plotter Jack Kirby.`
+  },
+  {
+    title: "Hulk",
+    src: "../imgs/hulk.jpg",
+    desc: `he Hulk is a fictional superhero appearing in American comic books published by Marvel Comics.
+    The character was created by Stan Lee and Jack Kirby, and first appeared in The Incredible Hulk #1 (May 1962).
+    Throughout his comic book appearances, the Hulk is portrayed as a large green humanoid that possesses immense superhuman strength and great invulnerability, attributes that grow more potent the angrier he becomes.
+    Hulk is the alter ego of Bruce Banner, a socially withdrawn and emotionally reserved physicist who physically transforms into the Hulk under emotional stress and other specific circumstances at will or against it; these involuntary transformations lead to many complications in Banner's life. When transformed, the Hulk often acts as a disassociated personality separate from Banner.`
+  }
+];
